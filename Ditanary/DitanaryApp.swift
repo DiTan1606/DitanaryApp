92eddf2413b99617dcae61cb309f1ffd0797b26a
ditan <23521406@gm.uit.edu.vn>
2026-04-24 00:13:51 +0700

    Initial Commit

---
+++ Ditanary/DitanaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DitanaryApp.swift
+//  Ditanary
+//
+//  Created by Trần Lê Duy Tân on 24/4/26.
+//
+
+import SwiftUI
+
+@main
+struct DitanaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DitanaryTests/DitanaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  DitanaryTests.swift
+//  DitanaryTests
+//
+//  Created by Trần Lê Duy Tân on 24/4/26.
+//
+
+import Testing
+@testable import Ditanary
+
+struct DitanaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
